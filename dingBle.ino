505732d0abe2676cf753e61e769e446f68cda76e
Sparkle <zanjie1999@yeah.net>
2021-03-01 21:47:09 +0800

    重写了，可以完美模拟出打卡机发射的蓝牙

--- dingBle.ino
+++ dingBle.ino
@@ -1,52 +1,69 @@
-#include "BluetoothSerial.h"
-#include "SimpleBLE.h"
 
-RTC_DATA_ATTR int addrIndex = 0;
-int macAddrsSize = 7;
-uint8_t macAddrs[][6] = {
-    {0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff}, 
-    {0x11, 0x22, 0x33, 0x44, 0x55, 0x66}};
-String bleName = "";
+#include "BLEDevice.h"
+#include "BLEUtils.h"
+#include "esp_sleep.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+uint8_t temprature_sens_read();
+//uint8_t g_phyFuns;
+
+#ifdef __cplusplus
+}
+#endif
+
+BLEAdvertising *pAdvertising;
+
+uint8_t bleMac[6] = {0x11, 0x22, 0x33, 0x44, 0x55, 0x66};
+uint8_t bleRaw[31] = {0x02, 0x01, 0x1A, 0x17, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03, 0x03, 0x3C, 0xFE};
 
 void setup() {
-    pinMode(0, INPUT);
-    Serial.begin(115200);
-    Serial.println("\r\n");
-    Serial.println(addrIndex);
+  Serial.begin(115200);
+
+  // esp32没有提供设置蓝牙mac地址的api 通过查看esp32的源代码
+  // 此操作将根据蓝牙mac算出base mac
+  if (UNIVERSAL_MAC_ADDR_NUM == FOUR_UNIVERSAL_MAC_ADDR) {
+    bleMac[5] -= 2;
+  } else if (UNIVERSAL_MAC_ADDR_NUM == TWO_UNIVERSAL_MAC_ADDR) {
+    bleMac[5] -= 1;
+  }
+  esp_base_mac_addr_set(bleMac);
+
+  // 初始化
+  BLEDevice::init("");
+
+  // Create the BLE Server
+  // BLEServer *pServer = BLEDevice::createServer(); // <-- no longer required to instantiate BLEServer, less flash and ram usage
+
+  pAdvertising = BLEDevice::getAdvertising();
+
+  // 里面有个 m_customScanResponseData = true; 和 m_customScanResponseData = true; 所以只能先随便设置一下
+//  BLEAdvertisementData oAdvertisementData = BLEAdvertisementData();
+  BLEAdvertisementData oScanResponseData = BLEAdvertisementData();
+//  pAdvertising->setAdvertisementData(oAdvertisementData);
+  pAdvertising->setScanResponseData(oScanResponseData);
+
+  // 简单粗暴直接底层api重新设置一下抓到的raw
+  esp_err_t errRc = ::esp_ble_gap_config_scan_rsp_data_raw(bleRaw, 31);
+  if (errRc != ESP_OK) {
+    Serial.printf("esp_ble_gap_config_scan_rsp_data_raw: %d\n", errRc);
+  }
+//  errRc = ::esp_ble_gap_config_adv_data_raw(bleRaw, 31);
+//  if (errRc != ESP_OK) {
+//    Serial.printf("esp_ble_gap_config_adv_data_raw: %d\n", errRc);
+//  }
+
+  pAdvertising->start();
 }
 
 void loop() {
-    uint8_t* addr = macAddrs[addrIndex];
-    // esp32没有提供设置蓝牙mac地址的api 通过查看esp32的源代码
-    // 此操作将根据蓝牙mac算出base mac
-    if (UNIVERSAL_MAC_ADDR_NUM == FOUR_UNIVERSAL_MAC_ADDR) {
-        addr[5] -= 2;
-    } else if (UNIVERSAL_MAC_ADDR_NUM == TWO_UNIVERSAL_MAC_ADDR) {
-        addr[5] -= 1;
-    }
-    esp_base_mac_addr_set(addr);
-    // SimpleBLE ble;
-    // ble.begin(bleName);
-    BluetoothSerial SerialBT;
-    SerialBT.begin(bleName);
-    while (digitalRead(0)) {
-        if (Serial.available()) {
-            SerialBT.write(Serial.read());
-        }
-        if (SerialBT.available()) {
-            Serial.write(SerialBT.read());
-        }
-    }
-    addrIndex++;
-    if (addrIndex >= macAddrsSize) {
-        addrIndex = 0;
-    }
-    while (!digitalRead(0)) {
-        Serial.println(addrIndex);
-        delay(100);
-    }
-
-    //由于无法重新初始化蓝牙，因此直接重启
-    esp_sleep_enable_timer_wakeup(0);
+  // 闪闪灯
+  Serial.println("Sparkle");
+  delay(1000);
+  // 20分钟去待机避免忘了关
+  if (millis() > 1200000) {
     esp_deep_sleep_start();
+  }
 }
